c61e23e433b5769bd6597decafc607f7798cf12f
Lv Lifeng <lvlifen9@gmail.com>
2023-01-22 12:48:18 +0800

    Fix fmt

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -67,7 +67,7 @@ func NewConfigFromFile(filename string) (Config, error) {
 		logger.Log.Infof("Config file %s does not exist, using the default config: eye/dependency-default.yaml", filename)
 
 		var depEyeAbsPath string
-		if depEyeAbsPath, err = exec.LookPath(os.Args[0]); err != nil && {
+		if depEyeAbsPath, err = exec.LookPath(os.Args[0]); err != nil {
 			return nil, err
 		}
 		if bytes, err = os.ReadFile(path.Join(getEyeAbsPath(depEyeAbsPath), "dependency-default.yaml")); err != nil && !os.IsNotExist(err) {
